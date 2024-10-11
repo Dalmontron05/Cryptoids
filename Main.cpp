fc3a76ec2688af7dc403d98c233e7111c5d7e575
Dalmontron05 <vexr2dts85338@gmail.com>
2024-10-10 18:24:50 -0700

    initial commit

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+//* Main C++ file
+
+//TODO: import color codes
+
+// Imports
+#include <iostream>
+#include <string>
+
+// #include "main.h"
+
+using namespace std;
+
+
+// Global Variables
+
+
+// Classes
+
+
+// Functions
+int main()
+{
+    cout << "Hello World!";
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cryptoids
 A CLI Turn-Based Combat Game
+
+> It's kinda like a pokemon game, but with various Cryptids like the Wendigo. The gameplay is also a bit more than just "which pokemon has the type advantage and better stats."
