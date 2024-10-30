d4120b731a3478cc23742d9aeb85045c1603f784
Dalmontron05 <vexr2dts85338@gmail.com>
2024-10-29 17:53:46 -0700

    added terminal colors

--- Cryptoids/Main.java
+++ Cryptoids/Main.java
@@ -1,4 +1,8 @@
 //* Main Java File
+package Cryptoids;
+
+import ANSI.ConsoleColors;
+
 // Imports
 // import java.util.Scanner;
 
@@ -7,6 +11,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        System.out.println(ConsoleColors.GREEN_BACKGROUND + "Hello World!");
     }
 }
