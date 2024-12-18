08095b0c26c9ab3326821ddf9fb676b66dcb31a0
Dalmontron05 <vexr2dts85338@gmail.com>
2024-12-18 01:27:25 -0700

    setup default decks

--- Cryptoids/Main.java
+++ Cryptoids/Main.java
@@ -1,6 +1,7 @@
 //* Main Java File
 package Cryptoids;
 
+import java.util.ArrayList;
 // Imports
 import java.util.Scanner;
 import ANSI.EscapeCodes;
@@ -38,26 +39,43 @@ public class Main
     
             // Objects
             // Cryptoids
-            Bigfoot bigfoot = new Bigfoot();
-            Mothman mothman = new Mothman();
+            // player 1 default deck
+            ArrayList<Cryptoid> p1DefaultDeck = new ArrayList<Cryptoid>() {{
+                Bigfoot Bigfoot1 = new Bigfoot();
+                Bigfoot Bigfoot2 = new Bigfoot();
+                Mothman Mothman1 = new Mothman();
+                add(Bigfoot1);
+                add(Bigfoot2);
+                add(Mothman1);
+            }};
+            // player 2 default deck
+            ArrayList<Cryptoid> p2DefaultDeck = new ArrayList<Cryptoid>() {{
+                Bigfoot Bigfoot1 = new Bigfoot();
+                Bigfoot Bigfoot2 = new Bigfoot();
+                Mothman Mothman1 = new Mothman();
+                add(Bigfoot1);
+                add(Bigfoot2);
+                add(Mothman1);
+            }};
     
             // Players
-            Player player1 = new Player(player1Name);
-            Player player2 = new Player(player2Name);
+            Player player1 = new Player(player1Name, p1DefaultDeck);
+            Player player2 = new Player(player2Name, p2DefaultDeck);
     
+            // * Man screw the tutorial for now
             // Asks the user if they need a tutorial
-            System.out.println("Do you need the tutorial? (true/false)");
+            // System.out.println("Do you need the tutorial? (true/false)");
     
-            playTutorial = in.nextBoolean(); // ? Scanner doesn't have way to grab a character specifically, so maybe refactor this later?
+            // playTutorial = in.nextBoolean(); // ? Scanner doesn't have way to grab a character specifically, so maybe refactor this later?
     
-            if (playTutorial == true)
-            {
-                System.out.println("Alright, this is how you play the game.");
-            }
-            else
-            {
+            // if (playTutorial == true)
+            // {
+            //     System.out.println("Alright, this is how you play the game.");
+            // }
+            // else
+            // {
                 System.out.println("Alright, let's get into the game.");
-            }
+            // }
 
             // Replay system
             System.out.println("Would you like to play again? (true/false)");
--- Cryptoids/Player.java
+++ Cryptoids/Player.java
@@ -1,15 +1,20 @@
+//* Base player class for both players.
+
 package Cryptoids;
+import Cryptoids.Creatures.*;
+import java.util.ArrayList;
 
 public class Player
 {
     // Attributes
     public String name;
-
+    public ArrayList<Cryptoid> deck;
 
     // Constructors
-    public Player(String name)
+    public Player(String name, ArrayList<Cryptoid> deck)
     {
         this.name = name;
+        this.deck = deck;
     }
 }
 
