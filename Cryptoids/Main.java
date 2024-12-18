9d67756c65f3a1328e270a9b475448da347783ab
Dalmontron05 <vexr2dts85338@gmail.com>
2024-12-17 23:53:18 -0700

    added replay do loop

--- Cryptoids/Main.java
+++ Cryptoids/Main.java
@@ -6,51 +6,71 @@ import java.util.Scanner;
 import ANSI.EscapeCodes;
 import Cryptoids.Creatures.*;
 
+// TODO: refactor the whole introduction and tutorial in other methods. when you make the replay do loop, exclude those during a 'replay.'
 
 public class Main
 {
-    
     public static void main(String[] args)
     {
-        clear();
-
-        Scanner in = new Scanner(System.in);
-
+        
         // Variables
         String player1Name, player2Name;
-
-
-        // Introduction
-        System.out.println("Hello, Welcome to Cryptoids! This is a local multiplayer 1v1 card game. You and a friend can make a deck of various cryptids you know and love like bigfoot and mothman.\nLet's start with your names.");
-
-        // Asks players for their names
-        System.out.println("Who is Player 1?");
-        player1Name = in.nextLine();
-
-        System.out.println("Who is Player 2?");
-        player2Name = in.nextLine();
+        Boolean playTutorial, willReplay;
         
-        clear();
+        // Lets the users replay the game
+        do
+        {
+            Scanner in = new Scanner(System.in);
+            clear();
+            willReplay = false;
 
+            // Introduction
+            System.out.println("Hello, Welcome to Cryptoids! This is a local multiplayer 1v1 card game. You and a friend can make a deck of various cryptids you know and love like bigfoot and mothman.\nLet's start with your names.");
+            
+            // Asks players for their names
+            System.out.println("Who is Player 1?");
+            player1Name = in.nextLine();
+    
+            System.out.println("Who is Player 2?");
+            player2Name = in.nextLine();
+            
+            clear();
+    
+            // Objects
+            // Cryptoids
+            Bigfoot bigfoot = new Bigfoot();
+            Mothman mothman = new Mothman();
+    
+            // Players
+            Player player1 = new Player(player1Name);
+            Player player2 = new Player(player2Name);
+    
+            // Asks the user if they need a tutorial
+            System.out.println("Do you need the tutorial? (true/false)");
+    
+            playTutorial = in.nextBoolean(); // ? Scanner doesn't have way to grab a character specifically, so maybe refactor this later?
+    
+            if (playTutorial == true)
+            {
+                System.out.println("Alright, this is how you play the game.");
+            }
+            else
+            {
+                System.out.println("Alright, let's get into the game.");
+            }
 
-        // Objects
-        // Cryptoids
-        Bigfoot bigfoot = new Bigfoot();
-        Mothman mothman = new Mothman();
+            // Replay system
+            System.out.println("Would you like to play again? (true/false)");
+            willReplay = in.nextBoolean();
 
-        // Players
-        Player player1 = new Player(player1Name);
-        Player player2 = new Player(player2Name);
-        
-        // Example battle
-        // System.out.println("\n\nLet's see an example battle before we jump in! We'll give " + player1Name + " bigfoot and " + player2Name + " mothman.\n");
-        // System.out.println(bigfoot.art);
-        // System.out.println(mothman.art);
-        
-        bigfoot.printArt();
-        mothman.printArt();
-
-        in.close();
+            // Replay catch cases
+            if (willReplay == false)
+            {
+                System.out.println("Exiting...");
+                System.exit(0);
+            }
+        }
+        while (willReplay == true);
     }
 
     private static void clear()
